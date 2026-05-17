60ecd6b008d242d17c00fa949ee517b984399652
justhens <justhens@gmail.com>
2026-05-17 18:11:18 -0400

    learning activities

---
+++ week02/scripts/prophets.js
@@ -0,0 +1,36 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let birthdate = document.createElement('p');
+        let birthplace = document.createElement('p');
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        birthdate.textContent = `Born: ${prophet.birthdate}`;
+        birthplace.textContent = `Birthplace: ${prophet.birthplace}`;
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
+        cards.appendChild(card);
+    });
+};
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayProphets(data.prophets);
+}
+
+getProphetData();
